bf28d1e2a2e253906ddd80cf1e5db3da02000e33
Jackson <deltarax@gmail.com>
2022-04-18 16:36:40 -0700

    Basic Main menu screen

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    backgroundColor: '#FFFFFF',
     scene: [ Menu, Play ]
 }
 
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,14 +8,38 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        // define keys
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
+      // menu text configuration
+      let menuConfig = {
+        fontFamily: 'Papyrus',
+        fontSize: '28px',
+        backgroundColor: '#FFFFFF',
+        color: '#0000FF',
+        align: 'right',
+        padding: {
+            top: 5,
+            bottom: 5,
+        },
+        fixedWidth: 0
+      }
+        
+      // show menu text
+      this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Benless Bunner', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2, 'Use arrowkeys to move & (F) to fire', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press fire to begin!', menuConfig).setOrigin(0.5);
+
+      // define keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+      keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+      keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+      keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
     }
 
     update() {
-
+      if (Phaser.Input.Keyboard.JustDown(keyF)) {
+        this.scene.start("playScene");   
+        console.log("Play!");
+      }
     }
 }
