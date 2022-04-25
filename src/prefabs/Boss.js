cf229908544aa790f1dc93cdb7f6216f371bf770
Jackson <deltarax@gmail.com>
2022-04-25 10:59:05 -0700

    Added three different attacks!

--- src/prefabs/Boss.js
+++ src/prefabs/Boss.js
@@ -31,9 +31,20 @@ class Boss extends Phaser.Physics.Arcade.Sprite {
 
         // Check if currently attacking, and if not start a random attack
         if (!this.attacking){
-            if ((this.numberOfTicks % 200) == 0){
+            if ((this.numberOfTicks % 100) == 0){
                 this.attacking = true
-                this.beam1Attack();
+
+                // randomly pick a number between 1-3, and make that attack
+                this.randomAttack = this.getRandomInt(0,3);
+                if (this.randomAttack == 0){
+                    this.beam1Attack();
+                } else if (this.randomAttack == 1){
+                    this.beam2Attack();
+                } else if (this.randomAttack == 2){
+                    this.beam3Attack();
+                } else {
+                    console.error("Trying to make a beam 4 attack?");
+                }
             }
         } else {
             this.attacking = false;
@@ -63,12 +74,64 @@ class Boss extends Phaser.Physics.Arcade.Sprite {
         }, null, this);
     }
 
+    // Beam 2 attack, middle of the screen
+    beam2Attack(){
+        console.log("beam 2 attack");
+
+        // add in the portal attack indicator
+        this.portal = this.scene.add.sprite(this.x-20, game.config.height/2, 'portal');
+
+        // start the attack after 1 seconds
+        this.clock = this.scene.time.delayedCall(1000, () => {
+
+            //create the beam and check for collisions
+            this.beam = this.scene.physics.add.sprite(0, game.config.height/2, 'bigBeam').setOrigin(0, 0.5);
+            this.scene.physics.add.collider(this.beam, player, this.attackCollision, null, this);
+
+            // end the attack after a 1 second delay
+            this.clock2 = this.scene.time.delayedCall(1000, () => {
+                this.beam.destroy();
+                this.portal.destroy();
+            }, null, this);
+        }, null, this);
+    }
+
+    // Beam 3 attack, lower third of the screen
+    beam3Attack(){
+        console.log("beam 3 attack");
+
+        // add in the portal attack indicator
+        this.portal = this.scene.add.sprite(this.x-20, game.config.height/4 + game.config.height/2, 'portal');
+
+        // start the attack after 1 seconds
+        this.clock = this.scene.time.delayedCall(1000, () => {
+
+            //create the beam and check for collisions
+            this.beam = this.scene.physics.add.sprite(0, game.config.height/4 + game.config.height/2, 'bigBeam').setOrigin(0, 0.5);
+            this.scene.physics.add.collider(this.beam, player, this.attackCollision, null, this);
+
+            // end the attack after a 1 second delay
+            this.clock2 = this.scene.time.delayedCall(1000, () => {
+                this.beam.destroy();
+                this.portal.destroy();
+            }, null, this);
+        }, null, this);
+    }
+
     // check if the player is dead and flag it
     attackCollision(){
         if(!this.playerDead){
             this.playerDead = true;
         }
 
+    }
+
+    // random number generator
+    // credit to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
     }
 
 
