fcefe002ca33ff62ce706870b97fe5d43d22e04b
Jackson <deltarax@gmail.com>
2022-05-02 12:47:29 -0700

    changed header a little bit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // Collaborators: Solomon Bell, Esmerelda Rangel, Jackson Gerald
 // Title: Benless Bunner
-// Date Completed: ???
+// Date Completed: 5/2/2022
 // Creative Tilt: Technically, Jackson created a system that perfectly fits our bullet hell experience. Particularly, the way he manages the bullets and bones is perfect.
-// We also enjoy that we took something like a bullet hell, made it endless, and tried to flip it on its head by making it very calm.
-// Aesthetically, Esme took our idea of having a calm, relaxing bullet hell and created the perfect art. The game is very calming to play, even when things start to feel chaotic.
+// In particular, one technical idea we're proud of is we needed a way to find out when the hairball attacks finish.
+// An out of the box solution we came up with is using an extra, unseen hairball as a tracker.
+// By checking when that last hairball leaves the bounds of the screen, we can ensure that the attack is finished before starting a new one.
+// We also enjoy that we took something like a bullet hell, made it endless, and tried to flip it on its head by making it very calm and cute!
+// Aesthetically, Esme took our idea of having a calm, relaxing bullet hell and created the perfect art. The game feels calming to play, even when things start to get chaotic.
+// Finally Solomon created a fantastic soundtrack from scratch that mixed the themes of a calm cute game + a frantic bullethell perfectly!
 
 let config = {
     type: Phaser.CANVAS,
